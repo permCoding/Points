986738d1763ac295344909ff57660950559a8409
Андрей Беляков <ttxiom@gmail.com>
2020-05-23 12:59:08 +0500

    step-02

--- Points/Program.cs
+++ Points/Program.cs
@@ -8,7 +8,13 @@ namespace Points
         static void Main()
         {
             Point point = new Point();
-            Console.WriteLine($"X = {point.X}; Y = {point.Y}");
+
+            List<Point> points = new List<Point>();
+            points.Add(point);
+            points.Add(point);
+            points.Add(point);
+
+            Console.WriteLine($"X = {points[0].X}; Y = {points[0].Y}");
 
             Console.ReadLine();
         }
