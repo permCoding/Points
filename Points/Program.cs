55a967c4d03d005a582f18dff48f8d1173f8e3fd
Андрей Беляков <ttxiom@gmail.com>
2020-05-23 13:36:16 +0500

    step-06

--- Points/Program.cs
+++ Points/Program.cs
@@ -7,15 +7,21 @@ namespace Points
     {
         static void Main()
         {
+            Random rnd = new Random();
             int countPoints = 5;
+            int width = 60, height = 25;
+            Console.WindowWidth = width; Console.WindowHeight = height;
 
             List<Point> points = new List<Point>();
             for (int i = 0; i < countPoints; i++)
-                points.Add(new Point());
-            points.RemoveAt(3);
-            points.RemoveAt(1);
-            for (int i = 0; i < countPoints; i++)
-                points.Add(new Point());
+            {
+                points.Add(
+                    new Point(
+                        rnd.Next(width),
+                        rnd.Next(height)
+                    )
+                );
+            }
 
             foreach (var p in points)
                 Console.WriteLine($"Id = {p.Id}; X = {p.X}; Y = {p.Y}");
