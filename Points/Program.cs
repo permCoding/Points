4c04fc81fffb4ee18643d820eba35c9e6d8412b7
Андрей Беляков <ttxiom@gmail.com>
2020-05-23 12:38:07 +0500

    Добавьте файлы проекта.

---
+++ Points/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Points
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Point point = new Point();
+            Console.WriteLine($"X = {point.X}; Y = {point.Y}");
+
+            Console.ReadLine();
+        }
+    }
+}
